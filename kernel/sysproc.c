200f56e9449829e7f8631a4f7ada93e4ff17a232
Zhao Baokun <1056144393@qq.com>
2024-06-30 19:19:55 +0800

    实现并通过了sysinfo系统调用的所有测试

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -101,5 +102,26 @@ sys_trace(void)
 {
   // 获取系统调用的参数
   argint(0, &(myproc()->trace_mask));
+  return 0;
+}
+
+uint64 sys_sysinfo(void)
+{
+  uint64 addr;
+  if(argaddr(0,&addr)<0)
+  {
+    return -1;
+  }
+
+  struct sysinfo si;
+  struct proc *p = myproc();
+  si.freemem = getfreemem();
+  si.nproc = getnproc();
+  
+  if(copyout(p->pagetable,addr,(char*)&si,sizeof(si))<0)
+  {
+    return -1;
+  }
+
   return 0;
 }

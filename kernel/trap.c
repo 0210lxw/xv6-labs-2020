bb049e8559695eede57166a15a5e4cb987d85084
Zhao Baokun <1056144393@qq.com>
2024-07-12 13:49:42 +0800

    test0测试通过

--- kernel/trap.c
+++ kernel/trap.c
@@ -77,8 +77,15 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
+  if(which_dev == 2){
+    if(++p->ticks_count==p->alarm_interval)
+    {
+      //更改陷阱帧中保留的程序计数器
+      p->trapframe->epc = (uint64)p->alarm_handler;
+      p->ticks_count = 0;
+    }
     yield();
+  }
 
   usertrapret();
 }

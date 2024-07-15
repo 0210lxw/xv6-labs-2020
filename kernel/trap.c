802ee5e923fba2fed68bf1a9c90d7dc3fb542ef3
Zhao Baokun <1056144393@qq.com>
2024-07-15 17:45:02 +0800

    延迟分配完成

--- kernel/trap.c
+++ kernel/trap.c
@@ -65,9 +65,37 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  } 
+  else if((which_dev = devintr()) != 0)
+  {
     // ok
-  } else {
+  } 
+  else if((r_scause()==13)||(r_scause()==15))
+  {
+    uint64 fault_va = r_stval();
+    uint64 pa = (uint64)kalloc();
+    if(pa==0)
+    {
+      p->killed = 1;
+    }
+    else if(fault_va>=p->sz||fault_va<=PGROUNDDOWN(p->trapframe->sp))
+    {
+      kfree((void *)pa);
+      p->killed = 1;
+    }
+    else
+    {
+      fault_va = PGROUNDDOWN(fault_va);
+      memset((void *)pa, 0, PGSIZE);
+      if(mappages(p->pagetable,fault_va,PGSIZE,pa,PTE_W|PTE_U|PTE_R)!=0)
+      {
+        kfree((void *)pa);
+        p->killed = 1;
+      }
+    }
+  }
+  else 
+  {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;

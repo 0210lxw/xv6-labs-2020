7592c5692c93b2aa98a8fefac25f08421c3d6703
Zhao Baokun <1056144393@qq.com>
2024-07-24 11:19:42 +0800

    调度实验第一个任务完成

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,30 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+//已保存用户上下文切换的寄存器
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context contex;        //用户进程上下文
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,7 +82,9 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    thread_switch((uint64)&t->contex, (uint64)&current_thread->contex);
+  }
+  else
     next_thread = 0;
 }
 
@@ -77,6 +98,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->contex.ra = (uint64)func;                  //设定函数返回地址
+  t->contex.sp = (uint64)t->stack + STACK_SIZE; //设定栈指针
 }
 
 void 
